bbdc0b4c49442be603b2bcbc388a549d94e751bd
Chunyu <4a037920@gmail.com>
2022-08-05 00:44:50 +0800

    8-4

--- 練習數據/Program.cs
+++ 練習數據/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,54 +12,20 @@ namespace 練習數據
     {
         static void Main(string[] args)
         {
-
-            List<string> words = TestHelper.ReadFile("测试文件1/双城记.txt");
-            Console.WriteLine($"單辭總數:{words.Count}");
-
-            LinkedList3Dictionary<string, int> dic = new LinkedList3Dictionary<string, int>();
-
-            Stopwatch t1 = new Stopwatch();
-            t1.Start();
-            foreach (var item in words)
+            SortedArray1<int> s = new SortedArray1<int>();
+            int[] nums = { 7, 9, 11, 5, 2, 1,36,44,11,12,12,18,42, 0 };
+            foreach (var item in nums)
             {
-                if (dic.Contains(item))
-                    dic.Set(item, dic.Get(item) + 1);
-                else
-                    dic.Add(item, 1);
+                s.Add(item);
             }
-            t1.Stop();
-            Console.WriteLine($"不同單辭總數: {dic.Count}");
-            Console.WriteLine($"city出現的詞頻: {dic.Get("city")}");
-            Console.WriteLine($"運行時間: {t1.ElapsedMilliseconds} ms");
+            Console.WriteLine(s);
+            s.Remove(0);
+            s.Remove(9);
+            s.Remove(11);
+            s.Remove(10000);
+            Console.WriteLine(s);
 
-
-            //List<string> words =TestHelper.ReadFile("测试文件1/双城记.txt");
-            //Console.WriteLine($"單辭總數:{words.Count}");
-
-            //Stopwatch s1 = new Stopwatch();
-            //LinkedList1Set<string> l = new LinkedList1Set<string>();
-            //s1.Start();
-            //foreach (var item in words)
-            //{
-            //    l.Add(item);
-            //}
-            //s1.Stop();
-            //Console.WriteLine($"不重複單詞總是:{l.Count}");
-            //Console.WriteLine($"耗時:{s1.ElapsedMilliseconds} ms");
-
-            //Stopwatch s2 = new Stopwatch();
-            //Console.WriteLine();
-            //Array1Set<string> a = new Array1Set<string>();
-            //s2.Start();
-            //foreach (var item in words)
-            //{
-            //    a.Add(item);
-            //}
-            //s2.Stop();
-            //Console.WriteLine($"不重複單詞總是:{a.Count}");
-            //Console.WriteLine($"耗時:{s2.ElapsedMilliseconds} ms");
         }
-
         static long TestQueue(IQueue<int> queue,int n)
         {
             Stopwatch s = new Stopwatch();
@@ -89,6 +56,48 @@ namespace 練習數據
             }
             s.Stop();
             return s.ElapsedMilliseconds;
+        }
+    }
+    class TestSearch
+    {
+        public static int[] ReadFile(string path)
+        {
+            FileStream fs = new FileStream(path, FileMode.Open);
+            StreamReader sr = new StreamReader(fs);
+            List<int> l = new List<int>();
+            while (!sr.EndOfStream)
+            {
+                string line= sr.ReadLine();
+                l.Add(int.Parse(line));
+            }
+            fs.Close();
+            sr.Close();
+            return l.ToArray();
+        }
+        public static int FullSearch(int target,int[] arr)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] == target)
+                    return i;
+            }
+            return -1;
+        }
+        public static int BinarySearch(int target,int[] arr)
+        {
+            int l = 0;
+            int r = arr.Length - 1;
+            while (l <= r)
+            {
+                int mid = (r - l) / 2 + l;
+                if (target == arr[mid])
+                    return mid;
+                else if (target < arr[mid])
+                    r = mid - 1;
+                else // target>arr[mid]
+                    l = mid + 1;
+            }
+            return -1;
         }
     }
 }
---
+++ 練習數據/SortedArray1.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 練習數據
+{
+    class SortedArray1<Key> where Key:IComparable<Key>
+    {
+        private Key[] keys;
+        private int N;
+
+        public int Count { get { return N; } }
+        public bool IsEmpty { get { return N == 0; } }
+
+        public SortedArray1(int capacity)
+        {
+            this.keys = new Key[capacity];
+            this.N = 0;
+        }
+        public SortedArray1() : this(10)
+        {
+
+        }
+        public int Rank(Key key)
+        {
+            int l = 0;
+            int r = N-1;
+            while (l <= r)
+            {
+                int mid = (r - l) / 2 + l;
+                if (key.CompareTo(keys[mid]) < 0)
+                    r = mid - 1;
+                else if (key.CompareTo(keys[mid]) > 0)
+                    l = mid + 1;
+                else // key.CompareTo(data[mid])==0
+                    return mid;
+            }
+            return l;
+        }
+        private void ResetCapacity(int capacity)
+        {
+            Key[] newKeys = new Key[capacity];
+            for (int i = 0; i < N; i++)
+                newKeys[i] = keys[i];
+            this.keys = newKeys; 
+        }
+        public void Add(Key key)
+        {
+            int index = Rank(key);
+            if (index < N && key.CompareTo(keys[index]) == 0)
+                return;
+            if (N == this.keys.Length)
+                ResetCapacity(2 * keys.Length);
+            for (int i = N - 1; i >= index;i--)
+                keys[i + 1] = keys[i];
+            keys[index] = key;
+            N++;
+        }
+        public void Remove(Key key)
+        {
+            int index = Rank(key);
+            //if(index<N&&key.CompareTo(keys[index])==0)
+            //{
+            //    for (int i = index; i < N - 1; i++)
+            //        keys[i] = keys[i + 1];
+            //    keys[N - 1] = default(Key);
+            //    N--;
+            //    if (N == this.keys.Length / 4)
+            //        ResetCapacity(keys.Length / 2);
+            //}
+            if (index == N || key.CompareTo(keys[index]) != 0)
+                return;
+            for (int i = index; i < N - 1; i++)
+                keys[i] = keys[i + 1];
+            keys[N - 1] = default(Key);
+            N--;
+            if (N == this.keys.Length / 4)
+                ResetCapacity(keys.Length / 2);
+        }
+        public override string ToString()
+        {
+            StringBuilder builder = new StringBuilder();
+            builder.Append($"Capacity: {this.keys.Length}, N: {this.N}");
+            builder.Append($"{Environment.NewLine}[ ");
+            for (int i = 0; i < N; i++)
+            {
+                builder.Append(keys[i]);
+                if (i < N - 1)
+                    builder.Append(", ");
+            }
+            builder.Append(" ]");
+            return builder.ToString();
+        }       
+    }
+}
